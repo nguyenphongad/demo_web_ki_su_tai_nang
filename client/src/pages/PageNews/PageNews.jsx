977f636c8c94de6bb39af9255aaac0991b9555c7
Banh Mi <longkim223300@gmail.com>
2023-10-18 00:59:29 +0700

    update_GDNews

--- client/src/pages/PageNews/PageNews.jsx
+++ client/src/pages/PageNews/PageNews.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { NewsItem } from '../../components/ComponentNews/NewsItem'
+import axios from 'axios'
 const PageNews = () => {
-    return (
-        <div>PageNews</div>
-    )
+	// const [data, setData] = useState([])
+	// useEffect(() => {
+	// 	fetchData()
+	// }, [])
+
+	// const fetchData = async () => {
+	// 	try {
+	// 		const response = await axios.get('LINK')
+	// 		setData(response.data)
+	// 		console.log(response.data)
+	// 	} catch (error) {
+	// 		console.error('Error fetching data:', error)
+	// 	}
+	// }
+	const ARRAY_NEWS = [
+		{
+			time: '2023-09-22T12:34:56Z',
+			title: 'Tin Tức Mẫu 1',
+			des: 'Vào ngày 22 tháng 09 năm 2023 vừa qua, Bộ môn Hệ thống thông tin đã tổ chức đón tiếp Đoàn tham quan Trường Đại học Trà Vinh đến Phòng thực hành H4 nhằm giao lưu và trao đổi kinh',
+			link: '#',
+			hot: true,
+		},
+		{
+			time: '2023-09-22T12:34:56Z',
+			title: 'Tin Tức Mẫu 2',
+			des: 'Vào ngày 22 tháng 09 năm 2023 vừa qua, Bộ môn Hệ thống thông tin đã tổ chức đón tiếp Đoàn tham quan Trường Đại học Trà Vinh đến Phòng thực hành H4 nhằm giao lưu và trao đổi kinh',
+			link: '#',
+			hot: false,
+		},
+		{
+			time: '2023-09-22T12:34:56Z',
+			title: 'Tin Tức Mẫu 3',
+			des: 'Vào ngày 22 tháng 09 năm 2023 vừa qua, Bộ môn Hệ thống thông tin đã tổ chức đón tiếp Đoàn tham quan Trường Đại học Trà Vinh đến Phòng thực hành H4 nhằm giao lưu và trao đổi kinh',
+			link: '#',
+			hot: true,
+		},
+		{
+			time: '2023-09-22T12:34:56Z',
+			title: 'Tin Tức Mẫu 4',
+			des: 'Vào ngày 22 tháng 09 năm 2023 vừa qua, Bộ môn Hệ thống thông tin đã tổ chức đón tiếp Đoàn tham quan Trường Đại học Trà Vinh đến Phòng thực hành H4 nhằm giao lưu và trao đổi kinh',
+			link: '#',
+			hot: false,
+		},
+	]
+	const sortedNews = [...ARRAY_NEWS].sort((a, b) => b.hot - a.hot)
+
+	const renderNews = sortedNews.map((item, index) => {
+		return <NewsItem key={index}>{item}</NewsItem>
+	})
+
+	return (
+		<div className="pageNews">
+			<header className="heading-4">Tin tức - Sự kiện</header>
+			<div>{renderNews}</div>
+		</div>
+	)
 }
 
 export default PageNews
